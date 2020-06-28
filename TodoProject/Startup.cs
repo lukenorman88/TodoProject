228e67a792b0e3deb18447b9911ec7d8217b241c
Luke <l.norman.dev@gmail.com>
2020-06-28 22:19:52 +0100

    Added connection string to appsettings and to start up

--- TodoProject/Startup.cs
+++ TodoProject/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TodoProject.DatabaseDataAccessLayer;
 
 namespace TodoProject
 {
@@ -23,7 +25,9 @@ namespace TodoProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            //registers db context with the system, configures to use sql server, configuration gets connection string.
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("default")));
+            services.AddControllersWithViews();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
