680bd7aa889dedb1529dc36dabd92cf8562d9ce5
Luke <l.norman.dev@gmail.com>
2020-06-28 21:49:10 +0100

    Added to do list and to do item.

---
+++ TodoProject/Models/ToDoList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace TodoProject.Models
+{
+    [Table("ToDoList")]
+    public class ToDoList
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime DateCreated { get; set; }
+
+        //Naviagation property - helps entity frameworl build db
+        public virtual IEnumerable<TodoItem> ToDoItems { get; set; }
+    }
+}
---
+++ TodoProject/Models/TodoItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+
+namespace TodoProject.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(125)]
+        public string Name { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public bool IsCompleted { get; set; }
+
+        public DateTime? DateCompleted { get; set; }
+
+        [Required]
+        [ForeignKey("ToDoList")]
+        public int ToDoListId { get; set; }
+
+        //Navigation Property - Helps entity framework build db
+        public virtual ToDoList ToDoList { get; set; }
+    }
+}
